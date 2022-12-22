0efccf9a3fd8310510ea727ae2dcad3bd67c4902
zombie <javierfrancisco@gmail.com>
2022-12-21 20:27:39 -0600

    Add component composition with results component

---
+++ src/Results.jsx
@@ -0,0 +1,25 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1> No Pets Found</h1>
+      ) : (
+        pets.map((pet) => {
+          return (
+            <Pet
+              key={pet.id}
+              animal={pet.animal}
+              name={pet.name}
+              breed={pet.breed}
+              images={`${pet.city}, ${pet.state}`}
+            />
+          );
+        })
+      )}
+    </div>
+  );
+};
+
+export default Results;
--- src/useBreedList.js
+++ src/useBreedList.js
@@ -3,28 +3,28 @@ import { useState, useEffect } from "react";
 const localCache = {};
 
 export default function useBreedList(animal) {
-  const [breedList, setBreedLit] = useState([]);
-  const [status, setStatus] = useState("unloading");
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
 
   useEffect(() => {
     if (!animal) {
-      setBreedLit([]);
+      setBreedList([]);
     } else if (localCache[animal]) {
-      setBreedLit(localCache[animal]);
+      setBreedList(localCache[animal]);
     } else {
       requestBreedList();
     }
 
     async function requestBreedList() {
-      setBreedLit([]); 
+      setBreedList([]);
       setStatus("loading");
- 
+
       const res = await fetch(
         `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
       );
       const json = await res.json();
       localCache[animal] = json.breeds || [];
-      setBreedLit(localCache[animal]);
+      setBreedList(localCache[animal]);
       setStatus("loaded");
     }
   }, [animal]);
